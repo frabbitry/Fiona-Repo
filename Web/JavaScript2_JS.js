570f579be6ae5e319bb5a7105df002fd4720d412
frabbitry <frabbitry@gmail.com>
2019-04-15 22:38:27 -0400

    Javascript 2

---
+++ Web/JavaScript2_JS.js
@@ -0,0 +1,18 @@
+function showTextTelemachiad(){
+    document.getElementById("telemachiadDesc").style.display = "block"
+    document.getElementById("wanderingsDesc").style.display = "none"
+    document.getElementById("homecomingDesc").style.display = "none"
+   
+}
+function showTextWanderings(){
+        document.getElementById("wanderingsDesc").style.display = "block"
+        document.getElementById("telemachiadDesc").style.display = "none"
+        document.getElementById("homecomingDesc").style.display = "none"
+
+}
+
+function showTextHomecoming() {
+        document.getElementById("homecomingDesc").style.display = "block"
+        document.getElementById("telemachiadDesc").style.display = "none"
+        document.getElementById("wanderingsDesc").style.display = "none"
+}
